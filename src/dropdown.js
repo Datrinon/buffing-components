87790093b926f5a6a181d66532909a25b4b86f0d
Datrinon <dtrinh089@gmail.com>
2021-09-08 16:58:46 -0700

    Add popin animation for dropdown menu

--- src/dropdown.js
+++ src/dropdown.js
@@ -105,7 +105,7 @@ export default class MenuBar {
     const dropDownArrow = document.createElement("i");
 
     this.#addClassesToElement(dropDownContainer, classNames, "menubar-dropdown");
-    dropDownLinks.classList.add("dropdown-links", "collapsed");
+    dropDownLinks.classList.add("dropdown-links");//, "collapsed");
     dropDownArrow.classList.add("arrow", "down");
 
     links.forEach((pair) => {
