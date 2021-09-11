ec5e096414c398ad27753a644e35d774b206c7c4
Datrinon <dtrinh089@gmail.com>
2021-09-10 20:47:30 -0700

    Create sliderPics.js
    This file contains an array of objects with paths, titles, and captions.

---
+++ src/sliderPics.js
@@ -0,0 +1,31 @@
+const base = "images/";
+
+const SLIDER_PICS = [
+  {
+    path: "butte.jpg",
+    title: "Butte",
+    caption: "Lincoln County, Wyoming"
+  },
+  {
+    path: "capitol-reef.jpg",
+    title: "Capitol Reef",
+    caption: "Torrey, Utah"
+  },
+  {
+    path: "grand-teton.jpg",
+    title: "Grand Teton",
+    caption: "Teton County, Wyoming"
+  },
+  {
+    path: "open-road.jpg",
+    title: "Jackson Lake",
+    caption: "Teton County, Wyoming"
+  },
+  {
+    path: "sandstone.jpg",
+    title: "Navajo Sandstone",
+    caption: "Glen Canyon, Nevada"
+  }
+];
+
+export { base, SLIDER_PICS };
